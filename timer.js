6deb3b2bd13d17b68b24301a86b3f5a1be3058bb
gabrielleong <gabrielleong97@gmail.com>
2016-02-23 09:30:47 +0800

    first commit

---
+++ timer.js
@@ -0,0 +1,31 @@
+// function pageLoad() {
+
+	// console.clear();
+	// console.log("hi!");
+	var seconds = 0;
+	var display = document.getElementById('timer');
+
+	var reset = document.getElementById('reset');
+	var start = document.getElementById('start');
+	var pause = document.getElementById('pause');
+
+	function startTimer() {
+		timerID = window.setInterval(updateTime, 1000);
+		pause.addEventListener("click",pauseTimer,false);
+	}
+
+	function updateTime() {
+    	seconds++;
+		display.innerHTML = "Time Elapsed: " + seconds;
+    }
+    function pauseTimer () {
+    	window.clearInterval(timerID);
+    }
+    function resetTimer() {
+    	seconds = 0;
+    	window.clearInterval(timerID);
+    	display.innerHTML = "Stop Watch";
+    }
+    start.addEventListener("click",startTimer);
+
+    reset.addEventListener("click",resetTimer,false);
